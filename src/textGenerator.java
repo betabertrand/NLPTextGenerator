cc909fc71fdb4a74535c8edc7c2a0bf8e55cffae
thenightowl00 <btsp60@yahoo.com>
2017-11-23 23:58:34 -0500

    makes minor style changes and outputs result to a text file :)

--- src/textGenerator.java
+++ src/textGenerator.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.PrintWriter;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -20,19 +21,24 @@ public class textGenerator {
 
         HashMap<String, ArrayList<String>> map = new HashMap<>(wordGram(text));
 
-        String word = generateNewText(map);
+        String newText = generateNewText(map);
 
-
-
+        try {
+            PrintWriter outFile = new PrintWriter("newText.txt");
+            outFile.println(newText);
+            outFile.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public static Map<String, ArrayList<String>> wordGram(String text) {
 
         Scanner scanner = new Scanner(text);
         Map<String, ArrayList<String>> wordMap = new HashMap<>();
-        String currentWord  = scanner.next(); //.replaceAll("[^a-zA-Z ]", "").toLowerCase();;
+        String currentWord = scanner.next(); //.replaceAll("[^a-zA-Z ]", "").toLowerCase();;
         String nextWord;
-        while (scanner.hasNext()) {
+        do {
             nextWord = scanner.next(); //.replaceAll("[^a-zA-Z ]", "").toLowerCase();
             if (!wordMap.containsKey(currentWord)) {
                 ArrayList<String> wordList = new ArrayList<>();
@@ -43,30 +49,20 @@ public class textGenerator {
                 wordList.add(nextWord);
                 wordMap.put(currentWord, wordList);
             }
-            currentWord = nextWord;//.replaceAll("[^a-zA-Z ]", "").toLowerCase();;
+            currentWord = nextWord; //.replaceAll("[^a-zA-Z ]", "").toLowerCase();; //Ue this to remove punctiation
 
-            if (!scanner.hasNext()) {
-                currentWord = nextWord;
-                if (!wordMap.containsKey(currentWord)) {
-                    ArrayList<String> wordList = new ArrayList<>();
-                    wordMap.put(currentWord, wordList);
-                } else {
-                    ArrayList<String> wordList = wordMap.get(currentWord);
-                    wordMap.put(currentWord, wordList);
-                }
+        } while (scanner.hasNext());
+        {
+            currentWord = nextWord;
+            if (!wordMap.containsKey(currentWord)) {
+                ArrayList<String> wordList = new ArrayList<>();
+                wordMap.put(currentWord, wordList);
+            } else {
+                ArrayList<String> wordList = wordMap.get(currentWord);
+                wordMap.put(currentWord, wordList);
             }
+
         }
-
-
-
-
-        for (String name : wordMap.keySet()) {
-            String key = name.toString();
-            ArrayList<String > list = wordMap.get(name);
-            String printList = Arrays.toString(list.toArray());
-            System.out.println("Key: " + key + " " + printList);
-        }
-
         return wordMap;
     }
 
@@ -75,7 +71,6 @@ public class textGenerator {
         Random random = new Random();
         List<String> keys = new ArrayList<>(map.keySet());
         String randomKey = keys.get(random.nextInt(keys.size()));
-        //System.out.println(randomKey);
         String currWord = randomKey;
         String textResult = "";
 
@@ -83,19 +78,17 @@ public class textGenerator {
             textResult = textResult + " " + currWord;
             currWord = map.get(currWord).get(random.nextInt(map.get(currWord).size()));
 
-        } while (!map.get(currWord).isEmpty()); {
+        } while (!map.get(currWord).isEmpty());
+        {
             textResult = textResult + " " + currWord;
         }
 
-        System.out.println(textResult);
-
-        return null;
+        return textResult;
     }
-
-
 }
 
 /*
+//Used to print the words and all the words that appear after in in the text.
 for (String name : wordMap.keySet()) {
             String key = name.toString();
             ArrayList<String > list = wordMap.get(name);
